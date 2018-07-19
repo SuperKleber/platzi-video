6754aef0a9f3f68232f3317269789ea7d3c4df5b
SuperKleber <alejandrofaesta@gmail.com>
2018-07-19 19:59:24 -0400

    logo-src-bug

--- src/pages/componentes/related.js
+++ src/pages/componentes/related.js
@@ -5,7 +5,7 @@ function Related(props)
 {
   return(
     <div className="Related">
-      <img src= "../../../dist/logo.0100fb37538b70aac2e7bb2ed0b96b15.png" alt="logo" width={250}/>
+      <img src= "../../../images/logo.png" alt="logo" width={250}/>
     </div>
 
   )
